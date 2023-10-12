f22ea715cb8df93fe49a7c4563b07fe824d3ea94
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-10-12 01:39:08 -0300

    config storybook

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Button from "./components/ui/Button";
+
+function App() {
+  return (
+    <div className="w-full h-screen  p-4 text-3xl font-bold underline">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Button/index.tsx
@@ -0,0 +1,9 @@
+
+
+const Button = () => {
+  return (
+    <div className="w-full p-4 bg-slate-400 rounded-xl">Button fdf</div>
+  )
+}
+
+export default Button
